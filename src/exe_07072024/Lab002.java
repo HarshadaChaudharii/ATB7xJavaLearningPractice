4a6a1f069a6ed35de4c2be888c9fbf90f4c7593e
mayurch12 <kalpesh.chaudharikt@gmail.com>
2024-07-23 19:59:12 +0530

    file are updated

--- src/exe_07072024/Lab002.java
+++ src/exe_07072024/Lab002.java
@@ -3,6 +3,8 @@ package exe_07072024;
 public class Lab002 {
     public static void main(String[] args) {
         //comment
+        // this program not get any output it only showing that we can use comment like this in program
+        //it is for dev or QA to understand what is going on
     }
 
 }
