f1a82f9e2ce29f1f8d085d49952bb96bb8bedd80
harshadachaudhari <harshu0210@gmail.com>
2024-07-31 10:53:09 +0530

    All file are updated

--- src/exe_07072024/Lab006.java
+++ src/exe_07072024/Lab006.java
@@ -1,5 +1,18 @@
 package exe_07072024;
 
 public class Lab006 {
-    
+    public static void main(String[] args) {
+        int a= 10;
+        byte b= 127;
+        char c= 'A';
+        boolean is_married = true;
+        boolean not_married = false;
+        System.out.println("Int value is = "+a);
+        System.out.println("Character value is = "+c);
+        System.out.println("Byte value is ="+b);
+        System.out.println("Boolean value for is married = "+is_married);
+        System.out.println("Boolean value for Not married = "+not_married);
+
+
+    }
 }
---
+++ src/exe_07072024/Lab008.java
@@ -0,0 +1,13 @@
+package exe_07072024;
+
+public class Lab008 {
+    public static void main(String[] args) {
+        // simple calculator
+        int i=30 , j=20;
+        System.out.println("Addition of this two no. is = "+(i+j));
+        System.out.println("Substraction of this two no. is = "+(i-j));
+        System.out.println("Multiplication of this two no. is = "+(i*j));
+        System.out.println("Division of this two no. is = "+(i/j));
+        System.out.println("Modulus of this two no. is = "+(i%j));
+    }
+}
